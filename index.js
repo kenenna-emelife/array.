04a6499a22f9a13b98b5823c3f9fa8125cc1a0db
kenenna-emelife <kenennaemelife@gmail.com>
2022-07-31 19:32:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let array = [1, 2, 3]
+index = 0, 1, 2
+array[2] = "Three"
+console.log(array)
+let newarray = [1,2,3,"lion"]
+console.log("new array")
+if (12) {
+    console.log("youe eligible for codeville")
+}
+let weather = "sunny"
+if (weather === "sunny") {
+
+    console.log("umbrella")
+}
+else if (weather = "rainy") {
+    console.log("go inside")
+} else {
+    console.log("go out")
+}
+let a = 0
+a++
+a = a + 1
+for (let count = 1; count < 13; count++) {
+    let display = count + "*" + 2 + "=" + result
+    console.log(display)
+}
---
+++ solution.js
@@ -0,0 +1,10 @@
+let catobject = {
+    name: "emelife kenenna",
+    age: "13",
+    career: "software developer",
+    isabillionaire: true
+}
+console.log("catobject")
+console.log(catobject["name"])
+console.log(catobject["age"])
+console.log(catobject["career"])
