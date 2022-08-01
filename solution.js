54b4b5855e7fb1c66feb8abec09a6e6dfcb74f57
kenenna-emelife <108738864+kenenna-emelife@users.noreply.github.com>
2022-08-01 13:19:10 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,10 +1,16 @@
 let catobject = {
-    name: "emelife kenenna",
-    age: "13",
-    career: "software developer",
+    name: "Emelife kenenna",
+    agegrade: "13-15",
+    career: "software developer and Electrical engineer",
+    hobby: "I likes fixing things ",
+    hobby2:"I likes developing websites",
+    hobby3:"i like inventing",
     isabillionaire: true
-}
+    }
 console.log("catobject")
 console.log(catobject["name"])
-console.log(catobject["age"])
+console.log(catobject["agegrade"])
 console.log(catobject["career"])
+console.log(catobject["hobby"])
+console.log(catobject["hobby2"])
+console.log(catobject["hobby3"])
